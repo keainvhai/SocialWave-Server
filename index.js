cd0832db1c0db303cfd314ab0121f64c94d64f70
Xue Fei <xue.f1@northeastern.edu>
2024-12-31 00:54:24 -0500

    Fixed All bugs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+require("dotenv");
+
 app.use(express.json());
 app.use(cors());
 
@@ -20,8 +22,13 @@ app.use("/auth", usersRouter);
 const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.listen.env.PORT || 3001, () => {
+      console.log("Server running on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
